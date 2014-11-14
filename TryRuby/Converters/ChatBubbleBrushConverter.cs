0bd0690f425b06700bd641e8cc50af5805ff2f08
Allen Lee <looplee@msn.com>
2014-11-14 13:48:27 +0800

    Change background color of sent chat bubble based on RGB of accent color instead of opacity. Change foreground color of chat bubble to white.

--- TryRuby/Converters/ChatBubbleBrushConverter.cs
+++ TryRuby/Converters/ChatBubbleBrushConverter.cs
@@ -4,21 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using System.Windows.Media;
 using TryRuby.Models;
 
 namespace TryRuby.Converters
 {
-    public class OpacityConverter : IValueConverter
+    public class ChatBubbleBrushConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
+            var accentBrush = (SolidColorBrush)App.Current.Resources["PhoneAccentBrush"];
+
             var messageKind = (ReplMessageKind)value;
             switch (messageKind)
             {
                 case ReplMessageKind.Sent:
-                    return .6;
+                    var accentColor = accentBrush.Color;
+                    return new SolidColorBrush(
+                        Color.FromArgb(
+                            accentColor.A,
+                            (byte)(accentColor.R / 2),
+                            (byte)(accentColor.G / 2),
+                            (byte)(accentColor.B / 2)));
                 case ReplMessageKind.Received:
-                    return 1;
+                    return accentBrush;
                 default:
                     throw new InvalidOperationException();
             }
